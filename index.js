0259c93dd517256669ec8f7ad6a97a0b54f72ea8
Sabbir Rahman <121510193+iconsabbir@users.noreply.github.com>
2023-06-20 23:25:19 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+
+
+// function startCoding(){
+//     console.log("I Am Programmer");
+// }
+
+// startCoding()
+// console.log("i have no life")
+// console.log("i have no life")
+// console.log("i have no life")
+// console.log("i have no life")
+// console.log("i have no life")
+
+// function bringBbq(taka){
+//     var priceOfBbq = 20;
+//     var qtyOfBbq = taka / priceOfBbq;
+//     return qtyOfBbq;
+// }
+
+// var totalBbq = bringBbq(100);
+// console.log(totalBbq);
+
+
+
+//  function someOfTowNamber(namber1,namber2){
+
+//      var total = namber1 + namber2;
+//      return total;
+//  }
+ 
+//  var result = someOfTowNamber(1,2)
+//  console.log(result);
+
+// var student ={
+//     orderId: 4154652,
+//     name:"sabbir",
+//     gander:"male",
+//     old:18
+// }
+// console.log(student);
+
+
+// var myCumputer = {
+//     monetir:"samsung",
+//     ssd:"128GB",
+//     prosassor:"rayzen5",
+//     price:500
+// }
+// console.log(myCumputer);
+// myCumputer["price"] =30000;
+// console.log(myCumputer)
